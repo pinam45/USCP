bc09d88a5556bab74edd05a699a7c291e3b32cee
Maxime Pinard <maxime.pin@live.fr>
2019-10-07 11:48:11 +0200

    Added CMake configuration for printer

---
+++ code/printer/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	//TODO
+	return EXIT_SUCCESS;
+}
