5611132ae84d7d9ea4bd07fa02fd21e24249d40a
Maxime Pinard <maxime.pin@live.fr>
2019-11-05 13:29:37 +0100

    Added memetic common serializable structs

---
+++ code/common/include/common/algorithms/memetic.hpp
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#ifndef USCP_COMMON_MEMETIC_HPP
+#define USCP_COMMON_MEMETIC_HPP
+
+#include "common/algorithms/rwls.hpp"
+
+#include <cstddef>
+#include <limits>
+#include <string>
+
+namespace uscp::memetic
+{
+	struct position_serial final
+	{
+		size_t generation = std::numeric_limits<size_t>::max();
+		uscp::rwls::position_serial rwls_cumulative_position;
+		double time = std::numeric_limits<double>::max();
+	};
+	void to_json(nlohmann::json& j, const position_serial& serial);
+	void from_json(const nlohmann::json& j, position_serial& serial);
+
+	struct config_serial final
+	{
+		position_serial stopping_criterion;
+		uscp::rwls::position_serial rwls_stopping_criterion;
+	};
+	void to_json(nlohmann::json& j, const config_serial& serial);
+	void from_json(const nlohmann::json& j, config_serial& serial);
+
+	struct report_serial final
+	{
+		solution_serial solution_final;
+		position_serial found_at;
+		config_serial solve_config;
+		std::string crossover_operator;
+	};
+	void to_json(nlohmann::json& j, const report_serial& serial);
+	void from_json(const nlohmann::json& j, report_serial& serial);
+} // namespace uscp::memetic
+
+#endif //USCP_COMMON_MEMETIC_HPP
---
+++ code/common/src/algorithms/memetic.cpp
@@ -0,0 +1,56 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#include "common/algorithms/memetic.hpp"
+
+void uscp::memetic::to_json(nlohmann::json& j, const uscp::memetic::position_serial& serial)
+{
+	j = nlohmann::json{
+	  {"generation", serial.generation},
+	  {"rwls_cumulative_position", serial.rwls_cumulative_position},
+	  {"time", serial.time},
+	};
+}
+
+void uscp::memetic::from_json(const nlohmann::json& j, uscp::memetic::position_serial& serial)
+{
+	j.at("generation").get_to(serial.generation);
+	j.at("rwls_cumulative_position").get_to(serial.rwls_cumulative_position);
+	j.at("time").get_to(serial.time);
+}
+
+void uscp::memetic::to_json(nlohmann::json& j, const uscp::memetic::config_serial& serial)
+{
+	j = nlohmann::json{
+	  {"stopping_criterion", serial.stopping_criterion},
+	  {"rwls_stopping_criterion", serial.rwls_stopping_criterion},
+	};
+}
+
+void uscp::memetic::from_json(const nlohmann::json& j, uscp::memetic::config_serial& serial)
+{
+	j.at("stopping_criterion").get_to(serial.stopping_criterion);
+	j.at("rwls_stopping_criterion").get_to(serial.rwls_stopping_criterion);
+}
+
+void uscp::memetic::to_json(nlohmann::json& j, const uscp::memetic::report_serial& serial)
+{
+	j = nlohmann::json{
+	  {"solution_final", serial.solution_final},
+	  {"found_at", serial.found_at},
+	  {"solve_config", serial.solve_config},
+	  {"crossover_operator", serial.crossover_operator},
+	};
+}
+
+void uscp::memetic::from_json(const nlohmann::json& j, uscp::memetic::report_serial& serial)
+{
+	j.at("solution_final").get_to(serial.solution_final);
+	j.at("found_at").get_to(serial.found_at);
+	j.at("solve_config").get_to(serial.solve_config);
+	j.at("crossover_operator").get_to(serial.crossover_operator);
+}
