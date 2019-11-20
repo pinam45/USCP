0af8135180a429bdb9990ef7ccc29db8322873e2
Maxime Pinard <maxime.pin@live.fr>
2019-11-20 15:00:38 +0100

    Added RWLS weights crossover operator keep

---
+++ code/solver/include/solver/algorithms/wcrossover/keep.hpp
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#ifndef USCP_KEEP_HPP
+#define USCP_KEEP_HPP
+
+#include "common/data/instance.hpp"
+#include "common/data/solution.hpp"
+#include "common/utils/random.hpp"
+
+#include <dynamic_bitset.hpp>
+
+#include <string_view>
+
+namespace uscp::wcrossover
+{
+	struct keep final
+	{
+		explicit keep(const uscp::problem::instance& problem_): problem(problem_)
+		{
+		}
+
+		std::vector<ssize_t> apply1(const std::vector<ssize_t>& a,
+		                            [[maybe_unused]] const std::vector<ssize_t>& b,
+		                            [[maybe_unused]] random_engine& generator) const noexcept
+		{
+			assert(a.size() == problem.points_number && b.size() == problem.points_number);
+			return a;
+		}
+
+		std::vector<ssize_t> apply2([[maybe_unused]] const std::vector<ssize_t>& a,
+		                            const std::vector<ssize_t>& b,
+		                            [[maybe_unused]] random_engine& generator) const noexcept
+		{
+			assert(a.size() == problem.points_number && b.size() == problem.points_number);
+			return b;
+		}
+
+		[[nodiscard]] static std::string_view to_string() noexcept
+		{
+			return "keep";
+		}
+
+		const uscp::problem::instance& problem;
+	};
+} // namespace uscp::wcrossover
+
+#endif //USCP_KEEP_HPP
