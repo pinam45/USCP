60a9de37bc0600d3fadd859feee1a28c7c4f8cd2
Maxime Pinard <maxime.pin@live.fr>
2019-11-20 15:00:17 +0100

    Added RWLS weights crossover operator average

---
+++ code/solver/include/solver/algorithms/wcrossover/average.hpp
@@ -0,0 +1,64 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#ifndef USCP_AVERAGE_HPP
+#define USCP_AVERAGE_HPP
+
+#include "common/data/instance.hpp"
+#include "common/data/solution.hpp"
+#include "common/utils/random.hpp"
+
+#include <dynamic_bitset.hpp>
+
+#include <string_view>
+
+namespace uscp::wcrossover
+{
+	struct average final
+	{
+		explicit average(const uscp::problem::instance& problem_): problem(problem_)
+		{
+		}
+
+		[[nodiscard]] std::vector<ssize_t> apply(const std::vector<ssize_t>& a,
+		                                         const std::vector<ssize_t>& b) const noexcept
+		{
+			std::vector<ssize_t> weights;
+			weights.reserve(problem.points_number);
+			for(size_t i = 0; i < problem.points_number; ++i)
+			{
+				weights.push_back((a[i] + b[i]) / 2);
+			}
+			return weights;
+		}
+
+		std::vector<ssize_t> apply1(const std::vector<ssize_t>& a,
+		                            const std::vector<ssize_t>& b,
+		                            [[maybe_unused]] random_engine& generator) const noexcept
+		{
+			assert(a.size() == problem.points_number && b.size() == problem.points_number);
+			return apply(a, b);
+		}
+
+		std::vector<ssize_t> apply2(const std::vector<ssize_t>& a,
+		                            const std::vector<ssize_t>& b,
+		                            [[maybe_unused]] random_engine& generator) const noexcept
+		{
+			assert(a.size() == problem.points_number && b.size() == problem.points_number);
+			return apply(a, b);
+		}
+
+		[[nodiscard]] static std::string_view to_string() noexcept
+		{
+			return "average";
+		}
+
+		const uscp::problem::instance& problem;
+	};
+} // namespace uscp::wcrossover
+
+#endif //USCP_AVERAGE_HPP
