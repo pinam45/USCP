7b335c20bb2b50bda9fff973f65556e0b75a5037
Maxime Pinard <maxime.pin@live.fr>
2020-01-06 16:49:04 +0100

    Added 2 STS instances

--- code/common/include/common/data/instances/sts.hpp
+++ code/common/include/common/data/instances/sts.hpp
@@ -24,18 +24,19 @@
 	      BKS,                                                                          \
 	      false,                                                                        \
 	      &uscp::problem::sts::read)
-#define STS_INSTANCES_TABLE_SIZE 9
-#define STS_INSTANCES_TABLE(ENTRY)                                        \
-	STS_ENTRY(ENTRY, "data.9", "STS9", 12, 9, 0.333f, 1, 1, 5)            \
-	STS_ENTRY(ENTRY, "data.15", "STS15", 35, 15, 0.2f, 1, 1, 9)           \
-	STS_ENTRY(ENTRY, "data.27", "STS27", 117, 27, 0.111f, 1, 1, 18)       \
-	STS_ENTRY(ENTRY, "data.45", "STS45", 330, 45, 0.067f, 1, 1, 30)       \
-	STS_ENTRY(ENTRY, "data.81", "STS81", 1080, 81, 0.037f, 1, 1, 61)      \
-	STS_ENTRY(ENTRY, "data.135", "STS135", 3015, 135, 0.022f, 1, 1, 103)  \
-	STS_ENTRY(ENTRY, "data.243", "STS243", 9801, 243, 0.012f, 1, 1, 198)  \
-	STS_ENTRY(ENTRY, "data.405", "STS405", 27270, 405, 0.007f, 1, 1, 335) \
-	STS_ENTRY(ENTRY, "data.729", "STS729", 88452, 729, 0.004f, 1, 1, 617)
-//	STS_ENTRY(ENTRY, "data.1215", "STS1215", 245835, 1215, 0.002f, 1, 1, 1063)
+#define STS_INSTANCES_TABLE_SIZE 11
+#define STS_INSTANCES_TABLE(ENTRY)                                             \
+	STS_ENTRY(ENTRY, "data.9", "STS9", 12, 9, 0.333f, 1, 1, 5)                 \
+	STS_ENTRY(ENTRY, "data.15", "STS15", 35, 15, 0.2f, 1, 1, 9)                \
+	STS_ENTRY(ENTRY, "data.27", "STS27", 117, 27, 0.111f, 1, 1, 18)            \
+	STS_ENTRY(ENTRY, "data.45", "STS45", 330, 45, 0.067f, 1, 1, 30)            \
+	STS_ENTRY(ENTRY, "data.81", "STS81", 1080, 81, 0.037f, 1, 1, 61)           \
+	STS_ENTRY(ENTRY, "data.135", "STS135", 3015, 135, 0.022f, 1, 1, 103)       \
+	STS_ENTRY(ENTRY, "data.243", "STS243", 9801, 243, 0.012f, 1, 1, 198)       \
+	STS_ENTRY(ENTRY, "data.405", "STS405", 27270, 405, 0.007f, 1, 1, 335)      \
+	STS_ENTRY(ENTRY, "data.729", "STS729", 88452, 729, 0.004f, 1, 1, 617)      \
+	STS_ENTRY(ENTRY, "data.1215", "STS1215", 245835, 1215, 0.002f, 1, 1, 1063) \
+	STS_ENTRY(ENTRY, "data.2187", "STS2187", 796797, 2187, 0.001f, 1, 1, 0)
 
 namespace uscp::problem::sts
 {
